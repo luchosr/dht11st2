263da0a0f82b63d157e7e3b57b8b53b5d1312c97
luciano.ramello <luciano.ramello@vass.es>
2021-01-20 16:17:50 +0100

    update server.py

--- http/server.py
+++ http/server.py
@@ -21,65 +21,6 @@ while True:
         "device":str(config.device_id),
         "timestamp":str(timestamp)
     }
-""" 
-    # If Enviromental
-    if config.enviromental:
-        build_json['iot2tangle'].append({
-            "sensor": "Enviromental",
-            "data": [{
-                "Pressure": str(press),
-                "Temp": str(temp)
-            },{
-                "Humidity": str(humidity)
-            }]
-        })
- """
-
-"""     if config.dht11:
-        build_json['iot2tangle'].append({
-            "sensor": "dht11",
-            "data": [{
-                "Pressure": str(press),
-                "Temp": str(temp)
-            },{
-                "Humidity": str(humidity)
-            }]
-        }) """
-
-""" 
-    #If Accelerometer
-    if config.accelerometer:
-        build_json['iot2tangle'].append({
-            "sensor": "Accel",
-            "data": [{
-                "x": str(x_accelerometer),
-                "y": str(y_accelerometer),
-                "z": str(z_accelerometer)
-            }]
-        })
-
-    # If Gyroscope
-    if config.gyroscope:
-        build_json['iot2tangle'].append({
-            "sensor": "Gyroscope",
-            "data": [{
-                "x": str(x_gyroscope),
-                "y": str(y_gyroscope),
-                "z": str(z_gyroscope)
-            }]
-        })
-
-    # If Magonetometer
-    if config.magnetometer:
-        build_json['iot2tangle'].append({
-            "sensor": "Magnetometer",
-            "data": [{
-                "x": str(x_compass),
-                "y": str(y_compass),
-                "z": str(z_compass)
-            }]
-        }) """
-
     # Set Json headers
     headers = {"Content-Type": "application/json"}
 
