822c6f87cfd9fae3d42f60e4ecd3c34e4e23f0a9
Luciano Ramello Benjamin <luciano.ramello@vass.es>
2021-01-21 16:03:12 +0100

    update libraries

--- http/hello.py
+++ http/hello.py
@@ -1,5 +1,37 @@
 from collections import OrderedDict
 import time
 import config
+import requests
+import json
+#import RPi.GPIO as GPIO
+import dht11
 
-print('hola');
+# while True:
+#   # Get Unix timestamp
+#     timestamp = int(time.time())
+timestamp = 'feliz cumpleaños'
+print('hola')
+
+build_json = {
+    "iot2tangle": [],
+    "device": str(config.device_id),
+    "timestamp": str(timestamp)
+}
+# Set Json headers
+headers = {"Content-Type": "application/json"}
+
+# Send Data to Json server
+try:
+    build_json = json.dumps(build_json)
+    r = requests.post(config.endpoint, data=build_json, headers=headers)
+    r.raise_for_status()
+    print(":: Sending datasets ::")
+    print("--------------------------------------------------------")
+    print(build_json)
+
+except:
+
+    print("No server listening at " + str(config.endpoint))
+
+    # Interval
+    time.sleep(config.relay)
